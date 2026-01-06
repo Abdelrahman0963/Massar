425f62a0cc7b80bd05f6bcb816cba8f1dd47bb14
Abdelrahman Sayed <110208261+Abdelrahman0963@users.noreply.github.com>
2026-01-06 04:48:50 +0200

    add Work section with animations and translations

---
+++ src/components/sections/Work.tsx
@@ -0,0 +1,50 @@
+import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
+
+const Work = () => {
+    const { t } = useTranslation();
+    return (
+        <section id="work" className="py-20!">
+            <div className="w-full flex items-center gap-4 flex-col">
+                <motion.nav
+                    initial={{
+                        scale: 0.7,
+                        opacity: 0,
+                        y: 40,
+                    }}
+                    whileInView={{
+                        scale: 1,
+                        opacity: 1,
+                        y: 0,
+                    }}
+                    viewport={{ once: true, amount: 0.2 }}
+                    transition={{
+                        duration: 0.6,
+                        ease: "easeOut",
+                    }}
+                    className="text-center space-y-6 px-4! bg-[#1a1a21]/80 backdrop-blur-lg border border-white/10 rounded-full py-3!" >
+                    <p className="text-white/60 text-sm md:text-[1rem]">{t("Our Approach")}</p>
+                </motion.nav>
+                <motion.div
+                    initial={{
+                        y: 20, opacity: 0
+                    }}
+                    whileInView={{
+                        y: 0, opacity: 1
+                    }}
+                    viewport={{ once: true, amount: 0.4 }}
+                    transition={{
+                        duration: 0.6,
+                        delay: 0.6,
+                        ease: "easeOut",
+                    }}
+                    className="flex flex-col items-center text-center gap-4">
+                    <h1 className="text-3xl md:text-5xl font-bold text-white mt-6 md:mt-12">{t("How We Work")}</h1>
+                    <p className="text-center md:text-[1.2rem] text-[0.8rem] text-white/60 mt-4!">{t("A simple and clear process to transform your idea into digital reality")}</p>
+                </motion.div>
+            </div>
+        </section>
+    )
+}
+
+export default Work
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/sections/Hero"
 import { lazy } from "react";
 const Services = lazy(() => import("@/components/sections/Services"));
+const Work = lazy(() => import("@/components/sections/Work"));
 const Home = () => {
     return (
         <div className="min-h-screen bg-[#0a0a0a] text-white md:px-24! px-4! py-3!">
             <Hero />
             <Services />
+            <Work />
         </div>
     )
 }
